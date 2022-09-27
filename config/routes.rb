141623f8790014b6023b3cef143ede75daf04dd2
juan sebastian quintero ramirez <juansebastianquintero01@gmail.com>
2022-09-26 20:58:04 -0500

    Service model

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :services, except: :update
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_235420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "pickup_time"
+    t.datetime "deliver_time"
+    t.string "payment_method"
+    t.string "pickup_address"
+    t.string "deliver_address"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
