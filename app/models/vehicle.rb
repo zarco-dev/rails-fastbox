be4d85f464754457352a8cb8c9b6397729ed045c
diego marin <dilema4000@gmail.com>
2022-09-26 20:58:02 -0500

    Create model Vehicle

---
+++ app/models/vehicle.rb
@@ -0,0 +1,7 @@
+class Vehicle < ApplicationRecord
+  belongs_to :user, persistent: true
+
+  validates :plate_number, presence: true, uniqueness: true
+  validates :vehicle_type, presence: true, length: { minimum: 4 }
+  validates :user_id, presence: true, uniqueness: true
+end
