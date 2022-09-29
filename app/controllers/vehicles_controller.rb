5ba51429c1fd5958e6667564aae4e9f694859556
diego marin <dilema4000@gmail.com>
2022-09-28 19:22:04 -0500

    Controller and view

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,23 @@
 class VehiclesController < ApplicationController
+  def new
+    @vehicle = Vehicle.new
+    authorize @vehicle
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
+    auhotize @vehicle
+
+  def edit
+    authorize @vehicle
+  end
+
+  def update
+    authorize @vehicle
+  end
+
+  def def destroy
+    authorize @vehicle
+  end
 end
