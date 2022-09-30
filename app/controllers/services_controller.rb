c6d7e5b4481f9328d3d63b5632ce52933a203fb0
juan sebastian quintero ramirez <juansebastianquintero01@gmail.com>
2022-09-30 09:16:16 -0500

    general services changes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,9 +1,27 @@
 class ServicesController < ApplicationController
-  def new
+  def show
+    @service = Service.find(params[:id])
+  end
 
+  def new
+    @service = Service.new
+    authorize @service
   end
 
   def create
+    @service = Service.new(service_params)
+    @service.user = current_user
+    authorize @service
+    if @service.save
+      redirect_to @service
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def service_params
+    params.require(:service).permit(:pickup_address, :deliver_address, :payment_method)
   end
 end
