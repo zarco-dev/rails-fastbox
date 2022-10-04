0c02e3ddaa9e0eb6f928d46fabc675bdc71db227
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-10-04 08:20:19 -0500

    Javascript Conflicts resolve

--- app/javascript/controllers/sign_up_controller.js
+++ app/javascript/controllers/sign_up_controller.js
@@ -1,36 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sign-up"
-let type_user = document.querySelector(".usertype");
+// let type_user = document.querySelector(".usertype");
 
 export default class extends Controller {
 
 
-  static targets = ["users", "typeuser", "user", "driver", "licensefield", "bonuscode", "marco"]
+  static targets = ["users", "typeuser", "user", "driver", "licensefield", "bonuscode", "marco", "usertype"]
 
   connect() {
     this.marcoTarget.classList.add("marco");
   }
 
   user() {
-      if (this.userTarget.value === "user") {
-        this.typeuserTarget.innerHTML = "Personal";
-        this.licensefieldTarget.classList.add("ocultar");
-        this.bonuscodeTarget.classList.remove("ocultar");
-        this.usersTarget.classList.add("ocultar");
-        this.marcoTarget.classList.remove("marco");
-      }
+    this.typeuserTarget.innerHTML = "Personal";
+    this.licensefieldTarget.classList.add("ocultar");
+    this.bonuscodeTarget.classList.remove("ocultar");
+    this.usersTarget.classList.add("ocultar");
+    this.marcoTarget.classList.remove("marco");
   }
 
   driver() {
-    if (this.driverTarget.value === "driver") {
-      this.typeuserTarget.innerHTML = "Conductor";
-      this.bonuscodeTarget.classList.add("ocultar");
-      this.licensefieldTarget.classList.remove("ocultar");
-      this.usersTarget.classList.add("ocultar");
-      this.marcoTarget.classList.remove("marco");
-      // type_user.value = "driver";
-      type_user.value = "driver";
-    }
+    this.typeuserTarget.innerHTML = "Conductor";
+    this.bonuscodeTarget.classList.add("ocultar");
+    this.licensefieldTarget.classList.remove("ocultar");
+    this.usersTarget.classList.add("ocultar");
+    this.marcoTarget.classList.remove("marco");
+    this.usertypeTarget.innerHTML = `<input required="required" value="driver" autocomplete="off" type="hidden" name="user[user_type]" id="user_user_type">`;
+    // type_user.value = "driver";
   }
 }
