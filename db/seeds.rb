904340f06be577a3756471f41306e67024eda140
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-10-03 19:15:44 -0500

    Comment seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "LukFaker::Name.first_namee", movie: movies.first)
-User.destroy_all
-Service.destroy_all
+# User.destroy_all
+# Service.destroy_all
 
-10.times do
-  User.create(
-    username: Faker::Name.first_name,
-    email: Faker::Internet.email,
-    password: "123456",
-    user_type: 'personal'
-  )
-end
+# 10.times do
+#   User.create(
+#     username: Faker::Name.first_name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     user_type: 'personal'
+#   )
+# end
 
-10.times do
-  Service.create(
-    payment_method: ['Tarjeta', 'Contra-entrega'].sample,
-    pickup_address: Faker::Address.street_address,
-    deliver_address: Faker::Address.street_address,
-    user_id: (1..10).to_a.sample
-  )
-end
+# 10.times do
+#   Service.create(
+#     payment_method: ['Tarjeta', 'Contra-entrega'].sample,
+#     pickup_address: Faker::Address.street_address,
+#     deliver_address: Faker::Address.street_address,
+#     user_id: (1..10).to_a.sample
+#   )
+# end
