3c6b9afc5ed106143da5c1dcad118eb167b6fbed
diego marin <dilema4000@gmail.com>
2022-10-05 13:02:04 -0500

    Dashboard 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :tracking ]
+  skip_before_action :authenticate_user!, only: [ :home, :tracking, :profile] #borrar luego profile
 
   def home
     @services = policy_scope(Service)
   end
 
   def tracking
+  end
 
+  # profile creado provisionalmente luego adherir al dashboard con lo realizado Juanse y profile en routes tambien
+
+  def profile
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "pages", to: "pages#tracking", as: :tracking
+  get "profile", to: "pages#profile", as: :profile # borrar luego esta ruta
   # Defines the root path route ("/")
   # root "articles#index"
 
